8f829baa357a962e6e705d30f3cf39fbf212d3fb
edgaby77 <edgarsanchezhernandez323@gmail.com>
2023-09-05 12:38:02 -0600

    ejercicio1 p03

---
+++ practicas/P03/practica3.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h2>Ejercicio1</h2>
+    <p>Determina cuál de las siguientes variables son válidas y explica por qué:</p>
+    <p>$_myvar, $_7var, myvar, $myvar, $var7, $_element1, $house*5</p>
+    <?php
+$_myvar;
+$_7var;
+//myvar;//invalida
+$myvar;
+$var7;
+$_element1;
+//$house*5; //invalida
+     echo'<ul>';
+     echo'<li>$_myvar es valida por que inicia con guion bajo</li>';
+     echo'<li>$_7var es valida por que inicia con guion bajo</li>';
+     echo'<li>$myvar es valida por que inicia con una letra</li>';
+     echo'<li>$var7 es valida por que inicia con una letra</li>';
+     echo'<li>$_element1 es valida por que inicia con guion bajo</li>';
+     echo'<li>$house*5 no es valida por que lleva un *</li>';
+     echo'<li>myvar no es valida por que no lleva $</li>';
+     
+
+     echo'</ul>';
+    ?>
+
+</body>
+</html>
